ed63ec364361d73d95a6a5d9815678b4ecddc5ec
NishchithaNithin <109577712+NishchithaNithin@users.noreply.github.com>
2022-07-19 13:02:24 +0530

    Update Prctice3Test.java

--- mavenProject/src/test/java/com/test/Prctice3Test.java
+++ mavenProject/src/test/java/com/test/Prctice3Test.java
@@ -6,9 +6,11 @@ import org.testng.annotations.Test;
 public class Prctice3Test 
 {
 	 @Test
+
 		public void test3() 
 	 {
-		 Reporter.log("test3",true);
+		 Reporter.log("all is well",true);
+		
 	 } 
 }
 
