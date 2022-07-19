00d1d78f98fd84435aeed3bafd40d46df96e3da8
Nithin <Nithin@DESKTOP-TKNJ4V5>
2022-07-19 12:24:14 +0530

    commit1

---
+++ mavenProject/src/test/java/com/test/Prctice3Test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Prctice3Test 
+{
+	 @Test
+		public void test3() 
+	 {
+		 Reporter.log("test3",true);
+	 } 
+}
+
+
